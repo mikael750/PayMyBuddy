d67e917f3824cb9992aa10598c1a28d528ceb2be
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-06-16 18:14:37 +0200

    MHE - Creation de la page Login

--- src/main/java/com/paymybuddy/application/configuration/SpringSecurityConfig.java
+++ src/main/java/com/paymybuddy/application/configuration/SpringSecurityConfig.java
@@ -2,25 +2,24 @@ package com.paymybuddy.application.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfiguration {
-
+/*
     /**
      * Store the user details
      *
      * @return InMemoryUserDetailsManager
-     */
+     *
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
         UserDetails user1 = User.withUsername("appUser")
@@ -32,10 +31,16 @@ public class SpringSecurityConfig extends WebSecurityConfiguration {
                 .roles("ADMIN","USER")
                 .build();
         return new InMemoryUserDetailsManager(user1, admin);
+    }*/
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     /**
-     * Secure the http access according to the roles
+     * Secure the http access
      *
      * @param http HttpSecurity
      * @return http
@@ -43,14 +48,16 @@ public class SpringSecurityConfig extends WebSecurityConfiguration {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
-                .antMatchers("/user").hasRole("USER")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .oauth2Login();
+        http.authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated()
+                ).formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                )
+        .exceptionHandling((exceptionHandling) -> exceptionHandling.accessDeniedPage("/403"));
+
         return http.build();
     }
 
--- src/main/java/com/paymybuddy/application/controllers/LoginController.java
+++ src/main/java/com/paymybuddy/application/controllers/LoginController.java
@@ -1,116 +1,12 @@
 package com.paymybuddy.application.controllers;
 
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.security.RolesAllowed;
-import java.security.Principal;
-import java.util.Map;
 
 @RestController
 public class LoginController {
-
-    public LoginController(OAuth2AuthorizedClientService authorizedClientService) {
-    }
-    @RolesAllowed("USER")
-    @RequestMapping("/**")
-    public String getUser()
-    {
-        return "Welcome User";
-    }
-
-    @RolesAllowed({"USER","ADMIN"})
-    @RequestMapping("/admin")
-    public String getAdmin()
-    {
-        return "Welcome Admin";
-    }
-    /**
-     * @param user
-     * @return
-     */
-    @RolesAllowed("USER")
-    @RequestMapping("/*")
-    public String getUserInfo(Principal user) {
-        StringBuilder userInfo= new StringBuilder();
-        if(user instanceof UsernamePasswordAuthenticationToken){
-            userInfo.append(getUsernamePasswordLoginInfo(user));
-        }
-        else if(user instanceof OAuth2AuthenticationToken){
-            userInfo.append(getOauth2LoginInfo(user));
-        }
-        return userInfo.toString();
-    }
-
-    /**
-     * @param user
-     * @return
-     */
-    private StringBuffer getUsernamePasswordLoginInfo(Principal user)
-    {
-        StringBuffer usernameInfo = new StringBuffer();
-
-        UsernamePasswordAuthenticationToken token = ((UsernamePasswordAuthenticationToken) user);
-        if(token.isAuthenticated()){
-            User u = (User) token.getPrincipal();
-            usernameInfo.append("Welcome, ").append(u.getUsername());
-        }
-        else{
-            usernameInfo.append("NA");
-        }
-        return usernameInfo;
-    }
-
-    /**
-     * @param user
-     * @return
-     */
-    private StringBuffer getOauth2LoginInfo(Principal user){
-
-        StringBuffer protectedInfo = new StringBuffer();
-
-        OAuth2AuthenticationToken authToken = ((OAuth2AuthenticationToken) user);
-        OAuth2User principal = ((OAuth2AuthenticationToken) user).getPrincipal();
-        //OAuth2AuthorizedClient authClient = this.authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
-
-        Map<String,Object> userAttributes = authToken.getPrincipal().getAttributes();
-
-        protectedInfo.append("Welcome, ").append(userAttributes.get("name")).append("<br><br>");
-        protectedInfo.append("e-mail: ").append(userAttributes.get("email")).append("<br><br>");
-        //String userToken = authClient.getAccessToken().getTokenValue();
-        //protectedInfo.append("Access Token: ").append(userToken).append("<br><br>");
-
-        OidcIdToken idToken = getIdToken(principal);
-        if(idToken != null) {
-
-            //protectedInfo.append("idToken value: ").append(idToken.getTokenValue()).append("<br><br>");
-            protectedInfo.append("Token mapped values <br><br>");
-
-            Map<String, Object> claims = idToken.getClaims();
-
-            for (String key : claims.keySet()) {
-                protectedInfo.append("  ").append(key).append(": ").append(claims.get(key)).append("<br>");
-            }
-        }
-
-        return protectedInfo;
-    }
-
-    /**
-     * @param principal
-     * @return
-     */
-    private OidcIdToken getIdToken(OAuth2User principal){
-        if(principal instanceof DefaultOidcUser oidcUser) {
-            return oidcUser.getIdToken();
-        }
-        return null;
+    @GetMapping(value =  {"/login"})
+    public String loginPage(){
+        return "login";
     }
 }
