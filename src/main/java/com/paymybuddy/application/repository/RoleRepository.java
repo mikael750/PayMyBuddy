b47642e01fa5453afb79542150685d97d11990d3
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-06-16 14:35:28 +0200

    MHE - SpringSecurityJwt Repository

--- src/main/java/com/paymybuddy/application/repository/RoleRepository.java
+++ src/main/java/com/paymybuddy/application/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.paymybuddy.application.repository;
 
-public interface RoleRepository {
+import com.paymybuddy.application.models.ERole;
+import com.paymybuddy.application.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
