6ba50f6d966e052a6cf35271c39fd5efd4acb01c
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-06-16 15:36:53 +0200

    MHE - SpringSecurityJwt Request/Response

--- src/main/java/com/paymybuddy/application/response/MessageResponse.java
+++ src/main/java/com/paymybuddy/application/response/MessageResponse.java
@@ -1,4 +1,14 @@
 package com.paymybuddy.application.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
 }
