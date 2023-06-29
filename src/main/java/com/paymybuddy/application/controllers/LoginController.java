9a675d96dcdc07635d4df195d3deceec2faef77c
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-06-29 20:37:15 +0200

    MHE - Creation du login page

--- src/main/java/com/paymybuddy/application/controllers/LoginController.java
+++ src/main/java/com/paymybuddy/application/controllers/LoginController.java
@@ -1,9 +1,9 @@
 package com.paymybuddy.application.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginController {
     @GetMapping(value =  {"/login"})
     public String loginPage(){
