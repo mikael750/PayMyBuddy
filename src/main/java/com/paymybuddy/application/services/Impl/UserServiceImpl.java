93a851e6d2d67b2547d76b040d347ff8153a3e06
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-07-09 20:18:40 +0200

    MHE - Creation methode impl findUserByEmail et saveUser

--- src/main/java/com/paymybuddy/application/services/Impl/UserServiceImpl.java
+++ src/main/java/com/paymybuddy/application/services/Impl/UserServiceImpl.java
@@ -1,12 +1,33 @@
 package com.paymybuddy.application.services.Impl;
 
+import com.paymybuddy.application.DTO.UserDTO;
+import com.paymybuddy.application.models.User;
 import com.paymybuddy.application.repository.UserRepository;
 import com.paymybuddy.application.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(UserDTO userDto) {
+        Optional<User> existingUser = findUserByEmail(userDto.getEmail());
+        if (existingUser.isPresent()){
+            throw new RuntimeException("Cette email = " + userDto.getEmail() + " est deja utiliser");
+        }
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        User userAccount = new User(userDto, passwordEncoder);
+        return userRepository.save(userAccount);
+    }
 }
