5a629fdd1bccf0a8203db0a1f6442f8debcd84e6
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-08-10 18:17:01 +0200

    MHE - PostMapping /registraion/save et son test

--- src/main/java/com/paymybuddy/application/controllers/AuthController.java
+++ src/main/java/com/paymybuddy/application/controllers/AuthController.java
@@ -1,13 +1,23 @@
 package com.paymybuddy.application.controllers;
 
 import com.paymybuddy.application.DTO.UserDTO;
+import com.paymybuddy.application.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping(value =  {"/login"})
     public ResponseEntity<String> loginPage(){
 
@@ -18,5 +28,21 @@ public class AuthController {
     public ResponseEntity<String> registrationPage(Model registrationFormDto){
         registrationFormDto.addAttribute("user", new UserDTO());
         return ResponseEntity.ok("registration");
+    }
+
+    @PostMapping("/registration/save")
+    public ResponseEntity<String> registration(@Valid @ModelAttribute("user")UserDTO userDTO, BindingResult result, Model registrationFormDto) {
+        if (result.hasErrors()) {
+            registrationFormDto.addAttribute("user", userDTO);
+            return ResponseEntity.ok("registration");
+        }
+
+        try {
+            userService.saveUser(userDTO);
+            return ResponseEntity.ok("redirect:/registration?success");
+        } catch (Exception e) {
+            result.rejectValue("email", null, "An account already exist with the email : " + userDTO.getEmail());
+            return ResponseEntity.ok("registration");
+        }
     }
 }
--- src/test/java/com/paymybuddy/application/AuthControllerTest.java
+++ src/test/java/com/paymybuddy/application/AuthControllerTest.java
@@ -1,14 +1,18 @@
 package com.paymybuddy.application;
 
+import com.paymybuddy.application.DTO.UserDTO;
 import com.paymybuddy.application.controllers.AuthController;
+import com.paymybuddy.application.models.User;
+import com.paymybuddy.application.services.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class AuthControllerTest {
@@ -17,6 +21,11 @@ public class AuthControllerTest {
     private AuthController authController;
     @Mock
     private Model model;
+    @Mock
+    private UserService userService;
+    @Mock
+    private BindingResult result;
+
 
     @Test
     public void loginPageTest(){
@@ -39,6 +48,28 @@ public class AuthControllerTest {
         String actualString = authController.registrationPage(model).toString();
 
         //THEN
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void registrationTest(){
+        //GIVEN
+        UserDTO userDTO = new UserDTO();
+        userDTO.setLastName("test");
+        userDTO.setFirstName("test");
+        userDTO.setEmail("test@test.test");
+        userDTO.setBirthdate("01/01/2000");
+        userDTO.setPassword("test");
+
+        String expectedString = "<200 OK OK,redirect:/registration?success,[]>";
+
+        when(userService.saveUser(userDTO)).thenReturn(new User());
+
+        //WHEN
+        String actualString = authController.registration(userDTO, result, model).toString();
+
+        //THEN
+        verify(userService, times(1)).saveUser(userDTO);
         assertEquals(expectedString, actualString);
     }
 }
