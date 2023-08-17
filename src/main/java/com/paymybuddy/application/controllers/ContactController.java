98e4e1cebfba60ca536c6492aeac1022d50adff6
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-08-17 14:40:54 +0200

    MHE - Creation addContact et son test + Clean up

--- src/main/java/com/paymybuddy/application/controllers/ContactController.java
+++ src/main/java/com/paymybuddy/application/controllers/ContactController.java
@@ -2,12 +2,18 @@ package com.paymybuddy.application.controllers;
 
 import com.paymybuddy.application.DTO.ContactDTO;
 import com.paymybuddy.application.services.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.security.Principal;
 
 @Controller
 public class ContactController {
@@ -22,4 +28,18 @@ public class ContactController {
         return ResponseEntity.ok("contact");
     }
 
+    @PostMapping("/contact")
+    public ResponseEntity<String> addContact(@Valid @ModelAttribute("add_contact") ContactDTO contactDTO, BindingResult result, Model contactFormDTO, Authentication principal){
+        if (result.hasErrors()) {
+            return contactPage(contactFormDTO, principal);
+        }
+        try {
+            userService.addContact(contactDTO, principal.getName());
+        } catch (Exception e) {
+            contactFormDTO.addAttribute("message_error", e.getMessage());
+            return contactPage(contactFormDTO, principal);
+        }
+
+        return ResponseEntity.ok("redirect:/contact?success");
+    }
 }
