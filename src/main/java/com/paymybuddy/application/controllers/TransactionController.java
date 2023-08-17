c7315bfd72c45563104950e3d3dacc4383948d95
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-08-17 20:51:57 +0200

    MHE - Creation transactionPage et son test

--- src/main/java/com/paymybuddy/application/controllers/TransactionController.java
+++ src/main/java/com/paymybuddy/application/controllers/TransactionController.java
@@ -1,8 +1,20 @@
 package com.paymybuddy.application.controllers;
 
+import com.paymybuddy.application.DTO.TransactionDTO;
 import com.paymybuddy.application.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TransactionController {
 
@@ -10,5 +22,17 @@ public class TransactionController {
     private TransactionService transactionService;
 
     @GetMapping(value = "/transaction")
-    public String transactionPage(){return "transaction";}
+    public ResponseEntity<String> transactionPage(Model transactionFormDTO, Authentication principal, @RequestParam(name = "page", required = false, defaultValue = "1") Integer currentPage, @RequestParam(name = "size", required = false, defaultValue = "5") Integer pageSize) {
+        Page<TransactionDTO> transactionDTOPage = transactionService.getPage(PageRequest.of(currentPage - 1, pageSize), principal.getName());
+        transactionFormDTO.addAttribute("transactionDTOPage", transactionDTOPage);
+
+        int totalPages = transactionDTOPage.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            transactionFormDTO.addAttribute("pageNumbers", pageNumbers);
+        }
+        return ResponseEntity.ok("transaction");
+    }
 }
---
+++ src/test/java/com/paymybuddy/application/TransactionControllerTest.java
@@ -0,0 +1,51 @@
+package com.paymybuddy.application;
+
+import com.paymybuddy.application.DTO.TransactionDTO;
+import com.paymybuddy.application.controllers.TransactionController;
+import com.paymybuddy.application.services.TransactionService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.Model;
+
+import java.math.BigDecimal;
+import java.security.Principal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class TransactionControllerTest {
+    @InjectMocks
+    private TransactionController transactionController;
+    @Mock
+    private TransactionService transactionService;
+    @Mock
+    private Model model;
+    @Mock
+    private Authentication principal;
+
+    @Test
+    public void transactionPageTest(){
+        //GIVEN
+        TransactionDTO transactionDto = new TransactionDTO( BigDecimal.ONE, "test", "test", "test",LocalDate.now());
+        Page<TransactionDTO> transactionDtoPage = new PageImpl(List.of(transactionDto), PageRequest.of(1,5), 1);
+        when(principal.getName()).thenReturn("test");
+        when(transactionService.getPage(PageRequest.of(0,5), "test")).thenReturn(transactionDtoPage);
+        String expectedString = "<200 OK OK,transaction,[]>";
+
+        //WHEN
+        String actualString = transactionController.transactionPage(model, principal, 1, 5).toString();
+
+        //THEN
+        assertEquals(expectedString, actualString);
+        verify(model, times(1)).addAttribute("pageNumbers", List.of(1,2));
+    }
+}
