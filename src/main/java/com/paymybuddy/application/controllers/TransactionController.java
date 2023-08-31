31113b985ed7409beb5209c87994294d50d53c07
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-08-31 19:50:34 +0200

    MHE - oublie @Controller à TransactionController

--- src/main/java/com/paymybuddy/application/controllers/TransactionController.java
+++ src/main/java/com/paymybuddy/application/controllers/TransactionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+@Controller
 public class TransactionController {
 
     @Autowired
