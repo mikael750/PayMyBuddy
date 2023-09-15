6400018cd171b346252adeeeea54e98f0a9f055c
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-09-15 16:19:46 +0200

    MHE - ajouter homeoff.html

--- src/main/java/com/paymybuddy/application/controllers/HomeController.java
+++ src/main/java/com/paymybuddy/application/controllers/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String homePage(){
         return "home";
     }
+
+    @GetMapping(value = "/homeoff")
+    public String homeOffPage(){
+        return "homeoff";
+    }
 }
