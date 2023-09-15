eb931497b270e1c117c681b04eea318e628512e8
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-09-15 17:07:00 +0200

    MHE - clean up

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ insert into user( FIRST_NAME, LAST_NAME, PASSWORD, EMAIL, BIRTHDATE, SOLDE) valu
 insert into user( FIRST_NAME, LAST_NAME, PASSWORD, EMAIL, BIRTHDATE, SOLDE) values ('Jean','Michel','$2a$12$jGKAC3h8riuul.nJEU723.TnDYjYePO3c1kfCaXj8nnQwrqD85GUq','jean@michel.fr','01/01/1991',0);
 insert into user( FIRST_NAME, LAST_NAME, PASSWORD, EMAIL, BIRTHDATE, SOLDE) values ('Marc','Sivel','$2a$12$jGKAC3h8riuul.nJEU723.TnDYjYePO3c1kfCaXj8nnQwrqD85GUq','marc@sivel.fr','02/02/1992',0);
 
-insert into transaction( AMOUNT, TRANSACTION_TYPE, SENDER_ID, RECEIVER_ID, DATE) values ( 50, 'RETRAIT', (SELECT id from user where email = 'rich@guy.fr'), (SELECT id from user where email = 'jean@michel.fr'), '2023-06-23');
-insert into transaction( AMOUNT, TRANSACTION_TYPE, SENDER_ID, RECEIVER_ID, DATE) values ( 50, 'DEPOT', (SELECT id from user where email = 'jean@michel.fr'), (SELECT id from user where email = 'rich@guy.fr'), '2023-06-23');
+insert into transaction( AMOUNT, TRANSACTION_TYPE, SENDER_ID, RECEIVER_ID, DATE) values ( 50, 'TRANSFERT', (SELECT id from user where email = 'rich@guy.fr'), (SELECT id from user where email = 'jean@michel.fr'), '2023-06-23');
+insert into transaction( AMOUNT, TRANSACTION_TYPE, SENDER_ID, RECEIVER_ID, DATE) values ( 50, 'TRANSFERT', (SELECT id from user where email = 'jean@michel.fr'), (SELECT id from user where email = 'rich@guy.fr'), '2023-06-23');
 
 insert into account( IBAN, BIC, AMOUNT) values ("FR4312739000302353653786L28","AXAFAKE",5000);
 insert into account( IBAN, BIC, AMOUNT) values ("FR3314508000401791339773H87","MMAFAKE",2000);
