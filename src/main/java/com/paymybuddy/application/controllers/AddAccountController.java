56c5dc8d4e545aaf3c2d5b97802c8f347f75bb0a
Mikael Heyraud <mikaelhey@hotmail.fr>
2023-09-17 22:23:41 +0200

    MHE - Amelioration addaccount

--- src/main/java/com/paymybuddy/application/controllers/AddAccountController.java
+++ src/main/java/com/paymybuddy/application/controllers/AddAccountController.java
@@ -1,7 +1,9 @@
 package com.paymybuddy.application.controllers;
 
 import com.paymybuddy.application.DTO.AccountDTO;
+import com.paymybuddy.application.models.User;
 import com.paymybuddy.application.services.AccountService;
+import com.paymybuddy.application.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,22 +14,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Optional;
+
 @Controller
 public class AddAccountController {
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private AccountService accountService;
 
     @GetMapping(value = {"/addaccount"})
-    public String addAccountPage(Model addAccountFormDto, AccountDTO accountDTO) {
+    public String addAccountPage(Model addAccountFormDto, Authentication principal) {
+        Integer userAccount =  userService.findUserByEmail(principal.getName()).get().getId();
+        addAccountFormDto.addAttribute("user_account", userAccount);
         addAccountFormDto.addAttribute("add_account", new AccountDTO());
         return "addaccount";
     }
 
 
     @PostMapping("/addaccount")
-    public String addAccount(@Valid @ModelAttribute("add_account") AccountDTO accountDTO, BindingResult result, Model addAccountFormDto) {
+    public String addAccount(@Valid @ModelAttribute("add_account") AccountDTO accountDTO, BindingResult result, Model addAccountFormDto, Authentication principal) {
         if (result.hasErrors()) {
+            Integer userAccount =  userService.findUserByEmail(principal.getName()).get().getId();
+            addAccountFormDto.addAttribute("user_account", userAccount);
             addAccountFormDto.addAttribute("add_account", accountDTO);
             return "addaccount";
         }
